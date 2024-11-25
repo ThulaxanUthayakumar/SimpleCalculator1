031f1c2e964db7931176b28c1a08b675afc5ff55
Thulaxan <Thulaxan@DESKTOP-N1DFVTC>
2024-11-25 17:40:06 +0530

    Add project files.

---
+++ SimpleCalculator1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SimpleCalculator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.operand1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.operand2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand 1";
+            // 
+            // operand1
+            // 
+            this.operand1.Location = new System.Drawing.Point(41, 73);
+            this.operand1.Name = "operand1";
+            this.operand1.Size = new System.Drawing.Size(732, 22);
+            this.operand1.TabIndex = 1;
+            this.operand1.TextChanged += new System.EventHandler(this.operand1_TextChanged);
+            this.operand1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.operand1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Operand 2";
+            // 
+            // operand2
+            // 
+            this.operand2.Location = new System.Drawing.Point(44, 166);
+            this.operand2.Name = "operand2";
+            this.operand2.Size = new System.Drawing.Size(729, 22);
+            this.operand2.TabIndex = 3;
+            this.operand2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.operand2_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(44, 211);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(350, 48);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(420, 211);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(353, 48);
+            this.btnSubstract.TabIndex = 5;
+            this.btnSubstract.Text = "Substract";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(44, 286);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(350, 42);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(420, 286);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(353, 42);
+            this.btnDivision.TabIndex = 7;
+            this.btnDivision.Text = "Division";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(44, 312);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 16);
+            this.result.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.operand2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.operand1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox operand1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox operand2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ SimpleCalculator1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void operand1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var operand1 = Convert.ToDouble(this.operand1.Text);
+            var operand2 = Convert.ToDouble(this.operand2.Text);
+
+            var result = operand1 + operand2;
+            this.result.Text = result.ToString();
+
+
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            var operand1 = Convert.ToDouble(this.operand1.Text);
+            var operand2 = Convert.ToDouble(this.operand2.Text);
+
+            var result = operand1 - operand2;
+            this.result.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            var operand1 = Convert.ToDouble(this.operand1.Text);
+            var operand2 = Convert.ToDouble(this.operand2.Text);
+
+            var result = operand1 * operand2;
+            this.result.Text = result.ToString();
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            var operand1 = Convert.ToDouble(this.operand1.Text);
+            var operand2 = Convert.ToDouble(this.operand2.Text);
+
+            var result = operand1 / operand2;
+            this.result.Text = result.ToString();
+        }
+
+        private void operand1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsControl(e.KeyChar)&& !char.IsDigit(e.KeyChar) && (e.KeyChar !='.'))
+            {
+                e.Handled = true;
+            }
+            if(e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void operand2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
